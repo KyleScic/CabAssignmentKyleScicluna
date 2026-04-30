b5d94a4b941de270a1454d57d915b3aae944d0ea
Kyle S <kyle.scicluna.e25329@mcast.edu.mt>
2026-04-30 10:44:38 +0200

    Fix: Moved apps inside express-monorepo and removed duplicate outer folder

--- apps/booking-service/routes/bookingRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import {createBooking, getBookings} from "../controller/bookingController.js";
-
-const router = express.Router();
-
-router.post('/',createBooking)
-router.get('/', getBookings);
-
-export default router;
--- apps/customer-service/models/notificationModel.js
+++
@@ -1,5 +0,0 @@
-const notificationSchema = new mongoose.Schema({
-    userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
-    message: { type: String, required: true },
-    read: { type: Boolean, default: false },
-}, { timestamps: true });
